542c403ae572869a12dd16d6f2330462a18db49d
Anchit Chandran (laptop) <anchit97123@gmail.com>
2023-03-24 21:25:25 +0000

    make times model

--- timeTrackerProj/timeTrack/models.py
+++ timeTrackerProj/timeTrack/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+from datetime import date
+
 # Create your models here.
+class Times(models.Model):
+    
+    date_of_work = models.DateField(verbose_name='Day of work', default=date.today())
+    start_time = models.TimeField(verbose_name='Start Time')
+    end_time = models.TimeField(verbose_name='End Time')
+    break_length = models.DurationField(verbose_name='Time taken for break')
+    
