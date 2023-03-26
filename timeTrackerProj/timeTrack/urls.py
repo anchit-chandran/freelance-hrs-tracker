8b7abc3c5398dd4301077bb0d0cb79bf49b7780b
Anchit <anchit97123@gmail.com>
2023-03-26 18:26:06 +0100

    adds nav and stylings

--- timeTrackerProj/timeTrack/urls.py
+++ timeTrackerProj/timeTrack/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.TimeListView.as_view(), name='index'),
     path('add-time', views.AddTimeView.as_view(), name='addTime'),
     path('update-time/<int:pk>', views.UpdateTimeView.as_view(), name='updateTime'),
+    path('delete-time/<int:pk>', views.DeleteTimeView.as_view(), name='deleteTime'),
 ]
--- timeTrackerProj/timeTrack/views.py
+++ timeTrackerProj/timeTrack/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from django.urls import reverse, reverse_lazy
 
 from django.views.generic.list import ListView
-from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 from timeTrack.models import Times
 
@@ -13,7 +13,7 @@ from timeTrack.forms import TimeCreateForm
 class TimeListView(ListView):
 
     model = Times
-    template_name = 'timeTrack/home.html'
+    template_name = 'timeTrack/main.html'
     context_object_name = 'logs'
 
 
@@ -29,4 +29,9 @@ class UpdateTimeView(UpdateView):
     model = Times
     template_name = 'timeTrack/time-update.html'
     form_class = TimeCreateForm
+    success_url = reverse_lazy('index')
+    
+class DeleteTimeView(DeleteView):
+    
+    model = Times
     success_url = reverse_lazy('index')
